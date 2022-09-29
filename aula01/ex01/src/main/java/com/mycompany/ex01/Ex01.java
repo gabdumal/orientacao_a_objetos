59c2b9efbf78174cf41f0594b797e5f5c6ebe639
Gabriel Malosto <professional@gabdumal.com>
2022-09-28 21:58:13 -0300

    Renomeia arquivos

--- aula01/ex01/src/main/java/com/mycompany/ex01/Ex01.java
+++ aula01/ex01/src/main/java/com/mycompany/ex01/Ex01.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-package com.mycompany.dcc025a01ex01;
+package com.mycompany.ex01;
 
 import java.util.Scanner;
 
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author ice
  */
-public class Dcc025a01ex01 {
+public class Ex01 {
 
     public static void main(String[] args) {
         System.out.println("Digite um valor em centavos: ");
