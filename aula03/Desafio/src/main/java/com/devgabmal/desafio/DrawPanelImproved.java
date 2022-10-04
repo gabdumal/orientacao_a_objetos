8a6457744fb92ac0a7a9809ebf51e863df32b7ba
Gabriel Malosto <professional@gabdumal.com>
2022-10-04 04:14:19 -0300

    Implementa resolução do desafio da aula 03

--- aula03/Desafio/src/main/java/com/devgabmal/desafio/DrawPanelImproved.java
+++ aula03/Desafio/src/main/java/com/devgabmal/desafio/DrawPanelImproved.java
@@ -31,5 +31,26 @@ public class DrawPanelImproved extends JPanel {
             y2 = (int) (height);
             g.drawLine(x1, y1, x2, y2);
         }
+        for (int i = 0; i < slices; i++) {
+            x1 = (int) (width);
+            y1 = (int) (i * sliceHeight);
+            x2 = (int) (i * sliceWidth);
+            y2 = 0;
+            g.drawLine(x1, y1, x2, y2);
+        }
+        for (int i = 0; i < slices; i++) {
+            x1 = 0;
+            y1 = (int) (height - (i * sliceHeight));
+            x2 = (int) (i * sliceWidth);
+            y2 = 0;
+            g.drawLine(x1, y1, x2, y2);
+        }
+        for (int i = 0; i < slices; i++) {
+            x1 = (int) width;
+            y1 = (int) (height - (i * sliceHeight));
+            x2 = (int) (i * sliceWidth);
+            y2 = (int) height;
+            g.drawLine(x1, y1, x2, y2);
+        }
     }
 }
