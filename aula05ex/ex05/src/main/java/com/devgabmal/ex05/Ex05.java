00fedcf577d6e5b2dc07d33fd5d61baadeb4a16a
Gabriel Malosto <professional@gabdumal.com>
2022-10-07 20:50:50 -0300

    Inicia resolução do ex05 da aula 05ex

---
+++ aula05ex/ex05/src/main/java/com/devgabmal/ex05/Ex05.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.devgabmal.ex05;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ice
+ */
+public class Ex05 {
+
+    public static double calculaInvestimento(double investimentoPorMes, double taxaMensal) {
+        double resultado = investimentoPorMes;
+
+        for (int mes = 12; mes > 0; mes--) {
+            double montante = resultado * (taxaMensal + 1) * mes;
+            resultado += montante - investimentoPorMes * mes;
+        }
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        double investimentoPorMes, taxaMensal;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("== Calculadora de Investimento ==");
+        System.out.print("  Quanto será investido por mês? ");
+        investimentoPorMes = Double.parseDouble(teclado.nextLine());
+        System.out.print("  Qual será a taxa de juros mensal? ");
+        taxaMensal = Double.parseDouble(teclado.nextLine().replace("%", "")) / (double) 100;
+
+        System.out.print("\n  Saldo do investimento após 1 ano: " + calculaInvestimento(investimentoPorMes, taxaMensal));
+    }
+}
